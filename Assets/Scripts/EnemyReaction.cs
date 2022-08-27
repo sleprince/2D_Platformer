3b294a041623157d977cd5311ced5ea915fd0d32
Stavrin <steffan.bculture@gmail.com>
2022-08-27 13:06:53 +0100

    Making the enivronment, player character and enemy. Added colliders and EnemyReaction script, level restarts when player hits enemy now.

---
+++ Assets/Scripts/EnemyReaction.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // This is very important if we want to restart the level
+public class EnemyReaction : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    // This function is called every time another collider overlaps the trigger collider
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Checking if the overlapped collider is an enemy
+        if (other.CompareTag("Enemy"))
+        {
+            // This scene HAS TO BE IN THE BUILD SETTINGS!!!
+            SceneManager.LoadScene("scene1");
+        }
+    }
+}
